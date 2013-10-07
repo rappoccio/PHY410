9ce56880e42736eac2dbf80d7cadc2c9baf374eb
Salvatore Rappoccio <rappoccio@gmail.com>
2013-10-07 17:34:20 -0400

    Typeo in bash script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export LD_LIBRARY_PATH=$PWD/cpt:$LD_LIBRARY_PATH
-export INCLUDEPATH=$PWD/cpt/$INCLUDEPATH
-export PYTHONPATH=$PWD/cpt_python:$PYTHON_PATH
+export LD_LIBRARY_PATH=$PWD/cpt
+export INCLUDEPATH=$PWD/cpt
+export PYTHONPATH=$PWD/cpt_python
 export PATH=$PWD/cpt:$PATH
