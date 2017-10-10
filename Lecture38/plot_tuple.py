0f8760b8d023d1cc74367f167e29f7ff459e00b3
Salvatore Rappoccio <rappoccio@gmail.com>
2017-10-10 15:25:38 -0400

    Adding plotting script

---
+++ Lecture38/plot_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from read_tuple import *
+import matplotlib.pyplot as plt
+import sys
+import numpy
+from matplotlib import legend
+import matplotlib.pyplot as plt
+
+if len(sys.argv) >= 2 :
+    x,y1,y2 = numpy.loadtxt(sys.argv[1], unpack=True)
+
+    ax = plt.subplot(1,1,1)
+    p1, = plt.plot(x,y1, '.')
+    p2, = plt.plot(x,y2)
+    ax.legend( [p1, p2], ["Numerical", "Exact"] )
+    plt.show()
