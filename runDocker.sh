47b6770f010aa733aabe5866bf923607e218b1ab
Salvatore Rappoccio <rappoccio@gmail.com>
2019-03-06 10:30:26 -0500

    Fixing x forwarding on mac

--- runDocker.sh
+++ runDocker.sh
@@ -10,7 +10,7 @@ fi
 if [ "$(uname)" == "Darwin" ]; then
     ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
     xhost + $ip
-    docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PWD}/../results:/results ${icommand} -p 8888:8888 $1
+    docker run --rm -it -e DISPLAY=$ip:0  -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PWD}/../results:/results ${icommand} -p 8888:8888 $1
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    docker run -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PWD}/../results:/results --rm -it --user $(id -u) "${icommand}" -p 8888:8888 $1 
+    docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PWD}/../results:/results --rm -it --user $(id -u) "${icommand}" -p 8888:8888 $1 
 fi
